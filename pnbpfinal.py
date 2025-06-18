c8adf28552933d7dea08d3c32915acd103b8a259
EkoSetiawan-cloud <ekosetiawan@students.amikom.ac.id>
2025-06-18 22:21:46 +0700

    Update pnbpfinal.py

--- pnbpfinal.py
+++ pnbpfinal.py
@@ -14,23 +14,33 @@ st.markdown("<h2 style='text-align: center;'>📊 Navigasi Aplikasi</h2>", unsaf
 
 menu = st.radio(
     "Pilih Modul:",
-    ["1. Modul Input", "2. Preprocessing & Agregasi", "3. Prediksi Holt-Winters", "4. Visualisasi", "5. Evaluasi","6. Export & Report Generator"],
-    horizontal=False,
-    index=0,
-    key="main_radio"
-) 
-# Routing modul
+    [
+        "1. Modul Input",
+        "2. Preprocessing & Agregasi",
+        "3. Prediksi Holt-Winters",
+        "4. Visualisasi",
+        "5. Evaluasi",
+        "6. Export & Report Generator"
+    ],
+    index=0
+)
+
+# Routing ke modul
 if menu == "1. Modul Input":
+    from Modul_Upload_Dataset import upload_dataset_page
     upload_dataset_page()
 elif menu == "2. Preprocessing & Agregasi":
+    from modul_preprocessing_agregasi import preprocessing_agregasi_page
     preprocessing_agregasi_page()
 elif menu == "3. Prediksi Holt-Winters":
+    from Modul_Prediksi import prediksi_holt_winters_page
     prediksi_holt_winters_page()
 elif menu == "4. Visualisasi":
+    from Modul_Visualisasi import visualisasi_prediksi_page
     visualisasi_prediksi_page()
 elif menu == "5. Evaluasi":
+    from Modul_Evaluasi import evaluasi_model_page
     evaluasi_model_page()
 elif menu == "6. Export & Report Generator":
-from Modul_Export import export_report_page
+    from Modul_Export import export_report_page
     export_report_page()
-
