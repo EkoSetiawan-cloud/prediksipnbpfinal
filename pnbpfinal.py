ce21a00ddeeafa017d2f73d37934e6e8da8fe44a
EkoSetiawan-cloud <ekosetiawan@students.amikom.ac.id>
2025-06-19 06:44:33 +0700

    Update pnbpfinal.py

--- pnbpfinal.py
+++ pnbpfinal.py
@@ -3,9 +3,10 @@ import streamlit as st
 # Impor semua modul halaman dengan nama file sesuai
 from Modul_Upload_Dataset import upload_dataset_page
 from modul_preprocessing_agregasi import preprocessing_agregasi_page
-from Modul_Prediksi import prediksi_holt_winters_page
+from Modul_Prediksi import prediksi_pnbp_page
 from Modul_Visualisasi import visualisasi_prediksi_page
 from Modul_Evaluasi import evaluasi_model_page
+from Modul_Export import export_report_page
 
 # Konfigurasi halaman Streamlit
 st.set_page_config(page_title="Prediksi PNBP", layout="centered")
@@ -17,7 +18,7 @@ menu = st.radio(
     [
         "1. Modul Input",
         "2. Preprocessing & Agregasi",
-        "3. Prediksi Double Smoothing",
+        "3. Prediksi Model",
         "4. Visualisasi",
         "5. Evaluasi",
         "6. Export & Report Generator"
@@ -32,9 +33,9 @@ if menu == "1. Modul Input":
 elif menu == "2. Preprocessing & Agregasi":
     from modul_preprocessing_agregasi import preprocessing_agregasi_page
     preprocessing_agregasi_page()
-elif menu == "3. Prediksi Holt-Winters":
-    from Modul_Prediksi import prediksi_holt_winters_page
-    prediksi_holt_winters_page()
+elif menu == "3. Prediksi Model":
+    from Modul_Prediksi import prediksi_pnbp_page
+    prediksi_pnbp_page()
 elif menu == "4. Visualisasi":
     from Modul_Visualisasi import visualisasi_prediksi_page
     visualisasi_prediksi_page()
