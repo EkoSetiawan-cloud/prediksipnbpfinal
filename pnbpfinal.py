90c3c5bc1c37a2677d3604ffbeb14bb4651296a0
EkoSetiawan-cloud <ekosetiawan@students.amikom.ac.id>
2025-06-19 05:33:38 +0700

    Update pnbpfinal.py

--- pnbpfinal.py
+++ pnbpfinal.py
@@ -5,7 +5,7 @@ from Modul_Upload_Dataset import upload_dataset_page
 from modul_preprocessing_agregasi import preprocessing_agregasi_page
 from Modul_Prediksi import prediksi_holt_winters_page
 from Modul_Visualisasi import visualisasi_prediksi_page
-from Modul_Evaluasi import evaluasi_model_page
+from Modul_Evaluasi import rediksi_pnbp_page
 
 # Konfigurasi halaman Streamlit
 st.set_page_config(page_title="Prediksi PNBP", layout="centered")
