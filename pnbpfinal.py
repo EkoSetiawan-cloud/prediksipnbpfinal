4c6b0249a6f1de71d4a2386aa4a6f85bb9a101f1
EkoSetiawan-cloud <ekosetiawan@students.amikom.ac.id>
2025-06-30 18:22:18 +0700

    Update pnbpfinal.py

--- pnbpfinal.py
+++ pnbpfinal.py
@@ -8,7 +8,6 @@ from login import login_page
 from Modul_Upload_Dataset import upload_dataset_page
 from modul_preprocessing_agregasi import preprocessing_agregasi_page
 from Modul_Prediksi_ARIMA_FINAL import prediksi_pnbp_page
-from Modul_Visualisasi import visualisasi_prediksi_page
 from Modul_Evaluasi import evaluasi_model_page
 from modul_analisa_final import kesimpulan_analisa_page
 from Modul_Export import export_report_page
@@ -23,10 +22,9 @@ menu = st.sidebar.radio("Pilih Modul", [
         "1. Modul Input",
         "2. Preprocessing & Agregasi",
         "3. Prediksi Model",
-        "4. Visualisasi",
-        "5. Evaluasi",
-        "6. Kesimpulan & Analisa",
-        "7. Export & Report Generator"
+        "4. Evaluasi",
+        "5. Kesimpulan & Analisa",
+        "6. Export & Report Generator"
     ],
     index=0
 )
@@ -38,8 +36,6 @@ elif menu == "2. Preprocessing & Agregasi":
     preprocessing_agregasi_page()
 elif menu == "3. Prediksi Model":
     prediksi_pnbp_page()
-elif menu == "4. Visualisasi":
-    visualisasi_prediksi_page()
 elif menu == "5. Evaluasi":
     evaluasi_model_page()
 elif menu == "6. Kesimpulan & Analisa":
